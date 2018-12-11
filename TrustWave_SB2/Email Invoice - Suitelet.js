d830f056ef40855ee471f48de6871840f0e359ec
russell.fulling <russell.fulling@CHI-FN-G6L6SQ2.trustwave.com>
2018-12-11 16:47:45 -0600

    new version with attachement

--- TrustWave_SB2/Email Invoice - Suitelet.js
+++ TrustWave_SB2/Email Invoice - Suitelet.js
@@ -2,7 +2,10 @@ function suitelet_print(request, response){
      var ifid = request.getParameter('custparam_recid');
 	 var record = nlapiLoadRecord('invoice', ifid);
 	 var toEmail = record.getFieldValue('custbody_to_email');	
-	 var createdFrom = record.getFieldValue('createdfrom');		 
+	 var createdFrom = record.getFieldValue('createdfrom');	
+	 
+	 var docName = record.getFieldValue('tranid');
+	 var mailDate= new Date();
 	 nlapiLogExecution('DEBUG', 'Emails: ', toEmail);	
 	 nlapiLogExecution('DEBUG', 'Created From SO Internal ID: ', createdFrom);	
      var file = nlapiPrintRecord('TRANSACTION', ifid, 'PDF',null); 
@@ -16,6 +19,7 @@ function suitelet_print(request, response){
 	  var records = new Array();
 	  records['transaction'] = nlapiGetRecordId(); //internal id of Transaction
 	  nlapiLogExecution('DEBUG', 'To Email', toEmail);	
+	 
 	  if (toEmail)
 	  {
 	  var cc = [];
@@ -23,7 +27,10 @@ function suitelet_print(request, response){
       for (var x = 0; x<toEmail.length; x++){
       cc[x] = toEmail[x];
       }
-	  var renderer = nlapiCreateTemplateRenderer();
+	  
+       
+      
+      var renderer = nlapiCreateTemplateRenderer();
 	  renderer.addRecord('transaction', record);
 	  nlapiLogExecution('DEBUG', 'Internal ID of Invoice 2', ifid);	
 	  renderer.setTemplate(emailSubj);	 
@@ -32,5 +39,27 @@ function suitelet_print(request, response){
 	  renderBody = renderer.renderToString();
 	  nlapiSendEmail(23779, cc, renderSubj, renderBody, null, null, records, file);
 	  nlapiLogExecution('DEBUG', 'Email successfully Sent');	  
+	 
+	  //RF to save pdf and put file on the transction.
+	  
+	  file.setName(docName + ' - '+  mailDate + '.pdf' );
+	  file.setFolder(603208);
+	  fileId = nlapiSubmitFile(file);
+	  
+	  //create the message so it stays on the transaction 
+	  
+	  var mailedMessage = nlapiCreateRecord('message');
+	  mailedMessage.setFieldValue('subject',renderSubj);
+	  mailedMessage.setFieldValue('message', renderBody);
+	  mailedMessage.setFieldValue('transaction', ifid);
+	  mailedMessage.setFieldValue('emailed','T');
+	  mailedMessage.setFieldValue('recipientemail',toEmail);
+	  var index = mailedMessage.getLineItemCount('mediaitem');
+	  nlapiLogExecution('DEBUG', 'what index '+index);
+	  mailedMessage.setLineItemValue('mediaitem', 'mediaitem', index + 1, fileId);
+	  
+	  
+	  nlapiSubmitRecord(mailedMessage);
+	  
 	  }
 }
